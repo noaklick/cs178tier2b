dffa2129c8b615d9f429762bbc14c8204eb88931
enscma2 <karina.halevy@gmail.com>
2023-03-27 23:44:23 -0400

    basic map loaded

--- src/App.js
+++ src/App.js
@@ -1,12 +1,43 @@
 import React, { useEffect, useState } from "react";
+import { GoogleMap, useJsApiLoader } from '@react-google-maps/api';
+import { myKey } from './config';
 
 function App() {
+
+    const containerStyle = {
+        width: '400px',
+        height: '400px'
+    };
+
+    const center = {
+        lat: -3.745,
+        lng: -38.523
+    };
     // Objects for each of the feeds we ingest from PassioGo endpoint
     const [feed, setFeed] = useState({
         serviceAlerts: {},
         tripUpdates: {},
         vehiclePositions: {}
     });
+
+    const { isLoaded } = useJsApiLoader({
+        id: 'google-map-script',
+        googleMapsApiKey: myKey
+    });
+
+    const [map, setMap] = React.useState(null);
+
+    const onLoad = React.useCallback(function callback(map) {
+        // This is just an example of getting and using the map instance!!! don't just blindly copy!
+        const bounds = new window.google.maps.LatLngBounds(center);
+        map.fitBounds(bounds);
+
+        setMap(map)
+    }, []);
+
+    const onUnmount = React.useCallback(function callback(map) {
+        setMap(null)
+    }, []);
 
     // Makes call to PassioGo endpoint and refreshes PassioGo feeds
     const refreshFeeds = async () => {
@@ -48,7 +79,16 @@ function App() {
 
     return (
         <div>
-            Hello World
+            <p>Hello World</p>
+            {isLoaded && (
+             <GoogleMap
+             mapContainerStyle={containerStyle}
+             center={center}
+             zoom={10}
+             onLoad={onLoad}
+             onUnmount={onUnmount}>
+             <></>
+             </GoogleMap>)}
         </div>
     );
 }
